b9d1de9e337a0017909cb4c9b51c7792d598c58e
Mohak Gadge <mohakgadge@gmail.com>
2020-02-27 00:47:05 +0530

    lexical analyser complete

--- lex.cpp
+++ lex.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
-#include<sstream>
 using namespace std;
 
 struct sym{
@@ -9,18 +8,41 @@ struct sym{
 };
 
 struct lit{
-	int add;
+    string name;
 };
 
 struct keyword{
 	string name;
 };
 
-int s_ind = 0,k_ind = 0;
+int s_ind = 0,k_ind = 0,l_ind = 0;
 int lex_ind= 0;
 struct sym s[10];
-struct keyword k[20];
+struct keyword k[30];
+struct lit l[20];
 
+bool check_lit(string& name){
+    for(int i = 0 ; i < l_ind ; i++){
+		if(name == l[i].name){
+			return false;
+		}
+	}
+	return true;
+}
+
+void put_lit(string& name){
+    if(check_lit(name)){
+        l[l_ind].name = name;
+        l_ind++;
+    }
+}
+
+void show_lit_tab(){
+	cout << "INDEX" << "\t\t" << "NAME\n"; 
+	for(int i = 0 ; i < l_ind ; i++){
+		cout << i << "\t\t" << l[i].name << endl;
+	}
+}
 
 void make_lex_file(){
 	ofstream f("lex.txt");
@@ -41,6 +63,31 @@ void put_keywords(){
 	put_key("return");
 	put_key("if");
 	put_key("else");
+    	put_key("printf");
+    	put_key("scanf");
+    	put_key("(");
+    	put_key(")");
+    	put_key("{");
+    	put_key("}");
+    	put_key(";");
+    	put_key(",");
+    	put_key("<");
+    	put_key(">");
+    	put_key("=");
+    	put_key(">=");
+    	put_key("<=");
+    	put_key("==");
+    	put_key("+");
+    	put_key("-");
+    	put_key("++");
+    	put_key("--");
+}
+
+void show_key_tab(){
+    cout << "INDEX" << "\t\t" << "NAME\n"; 
+	for(int i = 0 ; i < k_ind ; i++){
+		cout << i << "\t\t" << k[i].name << endl;
+	}
 }
 
 bool check_sym( string& a){
@@ -75,11 +122,38 @@ bool isKeyword(string& a){
 	return false;
 }
 
+int ret_index_sym(string a){
+    for(int i = 0; i < s_ind ; i++){
+		if(a == s[i].name){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int ret_index_lit(string a){
+    for(int i = 0; i < l_ind ; i++){
+		if(a == l[i].name){
+			return i;
+		}
+	}
+	return -1;
+}
+
+int ret_index_key(string a){
+    for(int i = 0; i < k_ind ; i++){
+		if(a == k[i].name){
+			return i;
+		}
+	}
+	return -1;
+}
+
 void fill_tab(string& a){
 	int i = 0;
 	string b,c,d;
-	if(a[0] == '"' || a[0] == '#'){
-		cout << a << endl;
+	if(a[0] == '#'){
+		//cout << a << endl;
 		return;
 	}
 	ofstream f("lex.txt",ios::app);
@@ -104,112 +178,104 @@ void fill_tab(string& a){
 					d.clear();
 				}
 				if(a[i] == '<' && (a[i+1] == '<' || a[i+1] == '=')){
-					d += a[i];
-					f << lex_ind << "\t\t" << d  << "\t\t" << "OPERATOR" <<"\n";
-					lex_ind++;
+					d += a[i+1];
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d  << "\t\t" << "TERMINAL" <<"\n";
 					d.clear();
 					i++;
 				}else if(a[i] == '>' && (a[i+1] == '>' || a[i+1] == '=')){
-					d += a[i];
-					f << lex_ind << "\t\t" << d << "\t\t" << "OPERATOR" <<"\n";
-					lex_ind++;
+					d += a[i+1];
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d << "\t\t" << "TERMINAL" <<"\n";
 					d.clear();
 					i++;
 				}else if(a[i] == '+' && (a[i+1] == '+' || a[i+1] == '=')){
-					d += a[i];
-					f << lex_ind << "\t\t" << d<< "\t\t" << "OPERATOR"  <<"\n";
-					lex_ind++;
+					d += a[i+1];
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d<< "\t\t" << "TERMINAL"  <<"\n";
 					d.clear();
 					i++;
 				}else if(a[i] == '-' && (a[i+1] == '-' || a[i+1] == '=')){
-					d += a[i];
-					f << lex_ind << "\t\t" << d << "\t\t" << "OPERATOR" <<"\n";
+					d += a[i+1];
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d << "\t\t" << "TERMINAL" <<"\n";
 					d.clear();
-					lex_ind++;
 					i++;
 				}
 				if(b.size() != 0 ){
-					f <<  lex_ind << "\t\t" << b << "\t\t";
 					if(isKeyword(b)){
+                        lex_ind = ret_index_key(b);
+                        f <<  lex_ind << "\t\t" << b << "\t\t";
 						f << "KEYWORD" << "\n";
 					}else{
+                        put_sym(b);
+                        lex_ind = ret_index_sym(b);
+                        f <<  lex_ind << "\t\t" << b << "\t\t";
 						f << "SYMBOL"<<"\n";
-						put_sym(b);
 					}
-					lex_ind++;
 					b.clear();
 				}
 				if(a[i] == ',' || a[i] == ';'){
 					//d += a[i];
-					f << lex_ind << "\t\t" << d << "\t\t" << "SEPERATOR" <<"\n";
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d << "\t\t" << "TERMINAL" <<"\n";
 					d.clear();
-					lex_ind++;
 					//i++;
-				}
-			//	cout << d << endl;
-				if(d.size() != 0){
-					f << lex_ind << "\t\t" << d << "\t\tOPERATOR" <<"\n";
+                }
+                if(a[i] == '(' && a[i+1] == '"'){
+					size_t found = a.find('"',i+2);
+                    lex_ind = ret_index_key(d);
+                    f << lex_ind << "\t\t" << d << "\t\tTERMINAL" <<"\n";
 					d.clear();
-					lex_ind++;
+                    if(found != string::npos){
+                        string temp = a.substr(i+2,found-i-2);
+                        put_lit(temp);
+                        lex_ind = ret_index_lit(temp);
+                        f << lex_ind << "\t\t" << temp << "\t\tLITERAL" << "\n";
+                        i = found;
+                    }
+				}
+				if(d.size() != 0){
+                    lex_ind = ret_index_key(d);
+					f << lex_ind << "\t\t" << d << "\t\tTERMINAL" <<"\n";
+					d.clear();
 				}
 				
 			}
 	}
 	if(b.size() != 0){
-		f << lex_ind << "\t\t" << b <<"\t\t" ;
+       
 		if(isKeyword(b)){
+            lex_ind = ret_index_key(b);
+		    f << lex_ind << "\t\t" << b <<"\t\t" ;
 			f << "KEYWORD" << "\n";
 		}else{
+            put_sym(b);
+            lex_ind = ret_index_sym(b);
+		    f << lex_ind << "\t\t" << b <<"\t\t" ;
 			f << "SYMBOL"<<"\n";
-			put_sym(b);
 		}
-		lex_ind++;
 	}
-	
 	f.close(); 
-	//cout << endl;
 }
 
 
 int main(){
 	ifstream f("input.c");
 	string a;
-	//for tokenizing
-	vector<string> tokens;
-	string intermediate;
 	put_keywords();
 	make_lex_file();
 	if(f.is_open()){
 		while(getline(f,a)){
-			stringstream check1(a);
-			while(getline(check1,intermediate,' ')){
-				//tokens.push_back(intermediate);
-				//cout << intermediate << " => ";
-				if(intermediate[0] == '"'){
-					/*string b = intermediate;
-					while(getline(check1,intermediate,' ')){
-						if(intermediate[0] == '"'){
-						cout << b << endl;
-							break;
-						}
-						b += ' ';
-						b += intermediate;
-					}*/
-					size_t found = a.find('"');
-					if(found != string::npos){
-					cout << a << "->" ;
-						cout << found << "->";
-						cout << a.substr(0,found) << endl;
-					}
-					//fill_tab(b);
-				}else{
-					//fill_tab(intermediate);
-				}
-				//cout << endl;
-			}
+           		 fill_tab(a);
 		}
 	}
-	//show_sym_tab();
+    	cout << "**SYMBOL TABLE**\n";
+	show_sym_tab();
+    	cout << "\n**LITERAL TABLE**\n";
+    	show_lit_tab();
+    	cout << "\n**KEYWORD TABLE**\n";
+    	show_key_tab();
 	f.close();
 	return 0;
 }
